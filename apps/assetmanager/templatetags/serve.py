2c06df45ea95b448051bb7bf1a1b205afa7b7e72
Ross Bruniges <rossbruniges@gmail.com>
2011-06-22 14:24:01 -0700

    Adding in runtime switch to settings.DEBUG on page render

--- apps/assetmanager/templatetags/serve.py
+++ apps/assetmanager/templatetags/serve.py
@@ -3,9 +3,13 @@ from django.conf import settings
 
 register = template.Library()
 
-@register.inclusion_tag('assetmanager/files.html')
-def serve(type, area):
-    is_dev = settings.DEBUG
+@register.inclusion_tag('assetmanager/files.html', takes_context = True)
+def serve(context, type, area):
+    request = context['request']
+    if request.GET.get('debug'):
+        is_dev = request.GET.get('debug')
+    else:
+        is_dev = settings.DEBUG
     if is_dev:
         files = settings.ASSETS[type][area]['dev']
     else:
