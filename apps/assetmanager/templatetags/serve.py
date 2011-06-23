c12f0ea5b8082966ce3eb2868aa66820b05775b8
Ross Bruniges <rossbruniges@gmail.com>
2011-06-22 20:32:51 -0700

    And probably using debug=True might not have been my best idea ever

--- apps/assetmanager/templatetags/serve.py
+++ apps/assetmanager/templatetags/serve.py
@@ -5,13 +5,11 @@ register = template.Library()
 
 @register.inclusion_tag('assetmanager/files.html', takes_context = True)
 def serve(context, type, area):
-    request = context['request'].GET.get('debug')
-    if request == "True":
+    request = context['request'].GET.get('files')
+    if request == "original":
         is_dev = True
     else:
         is_dev = settings.DEBUG
-
-    print is_dev
 
     if is_dev:
         files = settings.ASSETS[type][area]['dev']
