472ab417b1d8d83d88ec19e6f9ea2872bfa34598
Ross Bruniges <rossbruniges@gmail.com>
2011-06-22 20:30:41 -0700

    Only run the switch if the value if debug is set to 'True'

--- apps/assetmanager/templatetags/serve.py
+++ apps/assetmanager/templatetags/serve.py
@@ -5,11 +5,14 @@ register = template.Library()
 
 @register.inclusion_tag('assetmanager/files.html', takes_context = True)
 def serve(context, type, area):
-    request = context['request']
-    if request.GET.get('debug'):
-        is_dev = request.GET.get('debug')
+    request = context['request'].GET.get('debug')
+    if request == "True":
+        is_dev = True
     else:
         is_dev = settings.DEBUG
+
+    print is_dev
+
     if is_dev:
         files = settings.ASSETS[type][area]['dev']
     else:
