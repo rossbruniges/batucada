957cdba0d84229638a3d1f45e011778a22ea60ed
Ross Bruniges <rossbruniges@gmail.com>
2011-07-14 06:52:39 +0100

    [Bugzilla-668107] Images are now clickable without JS

--- apps/challenges/templatetags/challenge_images.py
+++ apps/challenges/templatetags/challenge_images.py
@@ -7,8 +7,17 @@ register = template.Library()
 @register.inclusion_tag('challenges/images.html')
 def challenge_images(haystack):
 
+    images = []
     soup = BeautifulSoup(haystack)
-    images = soup.findAll('img')
+    all_images = soup.findAll('img')
+
+    for i in all_images:
+        data = {
+            'src':i,
+            'url':i['src']
+        }
+        
+        images.append(data)
 
     return {
         'images':images
