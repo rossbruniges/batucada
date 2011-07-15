44ddbc073b511d0c70136e84f157a52dd4631ff4
Ross Bruniges <rossbruniges@gmail.com>
2011-07-15 16:39:07 +0100

    [Bugzilla-668107] Documenting a few of the hacks I had to ensure to get lightboxes working correctly

--- media/js/include/mojo_gallery.js
+++ media/js/include/mojo_gallery.js
@@ -1,19 +1,34 @@
+/**
+ * @namespace
+ * @requires jQuery, LABS.js, batucada.areas
+ * @returns batucada.mojo_challengs.init()
+ */
 batucada.mojo_winners=function() {
     var init, load_box;
+    /**
+     * Private function to load/set up colorbox plugin
+     * http://colorpowered.com/colorbox/
+     *
+     * @param context {jQuery object}
+     */
     load_box = function(context) {
         var boxCSS, attr_id, selector, attrs, tmp, num;
         attr_id = context.attr('id');
+        // grabbing all lightbox links 
         selector = '#' + attr_id + ' a[rel="note_group"]';
         attrs = {
             width:'80%',
             height:'80%',
             scrolling:true,
+            title:'Idea sketchnote(s)',
             scalePhotos:false,
             rel:attr_id
         };
         tmp = $(selector);
         num = tmp.length - 1;
+        // custom event to ensure that the page is ready once called
         context.bind('all_changed', function() {
+            // Loading in colourbox when asked for
             if ($.colorbox === undefined) {
                 boxCSS= $('<link />').appendTo($('head'));
                 boxCSS.attr({
@@ -25,10 +40,15 @@ batucada.mojo_winners=function() {
                 .wait(function() {
                     $('a[rel=' + attr_id  + ']').colorbox(attrs);
                 });
+                // but only the once!
             } else {
                 $('a[rel=' + attr_id + ']').colorbox(attrs);
             }
         })
+        /*
+         * This is rather hacky but the plugin deals with context awfully
+         * This allows us to set up a gallery of the photos we need, opposed to ALL photos
+         */
         tmp.each(function (index) {
             $(this).attr('rel',attr_id);
             if (index === num) {
@@ -36,6 +56,9 @@ batucada.mojo_winners=function() {
             }
         });
     }
+    /**
+     * Public function to set up page functionality
+     */
     init = function() {
         var galleries, len;
         galleries = $('div.photos');
@@ -49,11 +72,13 @@ batucada.mojo_winners=function() {
             trigger = $('<button>Show gallery</button>').appendTo(parent);
             trigger.click(function() {
                 current.slideDown(function() {
+                    // removing trigger as we won't need it now
                     trigger.remove();
                     load_box(current);
                 });
             });
             if (len === index) {
+                // sets visibility:visible on all nodes
                 $('html').addClass('photos_init');
             }
         });
