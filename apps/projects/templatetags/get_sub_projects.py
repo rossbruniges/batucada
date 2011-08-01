73ecc4c57ed250ccab017ffc58c027aa6d812297
Ross Bruniges <rossbruniges@gmail.com>
2011-08-01 14:59:37 +0100

    [Bugzilla-667628] Add in sub-project button now only displays for a logged in user

--- apps/projects/templatetags/get_sub_projects.py
+++ apps/projects/templatetags/get_sub_projects.py
@@ -3,12 +3,13 @@ from projects.models import Project
 
 register = template.Library()
 
-@register.inclusion_tag('projects/sub_projects.html')
-def get_sub_projects(project_id, allowed, id):
+@register.inclusion_tag('projects/sub_projects.html', takes_context=True)
+def get_sub_projects(context, project_id, allowed, id):
     subs = Project.objects.filter(parent_project_id=project_id)
 
     return {
         'sub_projects' : subs,
         'allowed_subs' : allowed,
-        'id': id
+        'id': id,
+        'logged_in' : context['user']
     }
